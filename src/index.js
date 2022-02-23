c4c7da8662b66d4dce78e611d4a0b780d1d9d9aa
stujlimaan <tujlimaan1999@gmail.com>
2022-02-23 22:44:54 +0530

    initial

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser=require('body-parser');
+
+const route = require('./routes/route');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use('/',route);
+
+const port = process.env.PORT || 3000;
+app.listen(port,function(){
+    console.log(`sever is running at ${port}`)
+})
